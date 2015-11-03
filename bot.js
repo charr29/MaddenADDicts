868fe0c6d409f771b2bf64304bf4308a77c2d55e
charr29 <charr29@gmail.com>
2015-11-03 18:49:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ function respond() {
     
  else if(request.text && botRegexRK.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/FM/standings/rankings");
+    postMessage("http://daddyleagues.com/FM/standings/ranking");
     this.res.end();
   } 
     
